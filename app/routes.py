1ed886ba92d15b412a5c38cb5c27b2f39d34940f
GAMING PC - k1r <secdev.kir@gmail.com>
2024-02-26 21:32:45 +0800

    done

--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,8 @@ main = Blueprint('main', __name__, template_folder='templates')
 # def home_route():
 #     return render_template('home.html')
 
-target_time = datetime.datetime(2024, 2, 29, 15, 0, 0)
+# target_time = datetime.datetime(2024, 2, 29, 15, 0, 0)
+target_time = datetime.datetime(2024, 2, 26, 21, 32, 20)
 
 @main.route('/')
 def index():
@@ -18,8 +19,10 @@ def index():
 def get_remaining_time():
     current_time = datetime.datetime.now()
     remaining_time = target_time - current_time
+
+    remaining_seconds = max(remaining_time.total_seconds(), 0)
     
-    days, seconds = divmod(remaining_time.total_seconds(), 86400)
+    days, seconds = divmod(remaining_seconds, 86400)
     hours, remainder = divmod(seconds, 3600)
     minutes, seconds = divmod(remainder, 60)
 
@@ -29,6 +32,10 @@ def get_remaining_time():
         'minutes': f'{int(minutes):02d}',
         'seconds': f'{int(seconds):02d}',
     })
+
+@main.route('/done')
+def done():
+    return render_template('done.html')
 
 @main.route('/test')
 def test():
