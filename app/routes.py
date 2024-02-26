5e50cb15b643a83ee91780ce702ed0046d5d29f0
dev-kir <secdev.kir@gmail.com>
2024-02-26 14:10:17 +0800

    done maybe

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import Flask, Blueprint, render_template, redirect, url_for, request
+from flask import Flask, Blueprint, render_template, redirect, url_for, request, jsonify
+import datetime
 
 main = Blueprint('main', __name__, template_folder='templates')
 # home = Blueprint('home', __name__, template_folder='templates')
@@ -7,9 +8,27 @@ main = Blueprint('main', __name__, template_folder='templates')
 # def home_route():
 #     return render_template('home.html')
 
+target_time = datetime.datetime(2024, 2, 29, 15, 0, 0)
+
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/get_remaining_time')
+def get_remaining_time():
+    current_time = datetime.datetime.now()
+    remaining_time = target_time - current_time
+    
+    days, seconds = divmod(remaining_time.total_seconds(), 86400)
+    hours, remainder = divmod(seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+
+    return jsonify({
+        'days': f'{int(days):02d}',
+        'hours': f'{int(hours):02d}',
+        'minutes': f'{int(minutes):02d}',
+        'seconds': f'{int(seconds):02d}',
+    })
 
 @main.route('/test')
 def test():
